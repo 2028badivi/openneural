5e9befac5131749ba0116ad07428eccf587d25d4
IndigoAtlas <bhavesh.adivi25@gmail.com>
2026-02-14 00:22:17 -0500

    Update base path for openneural repo

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/site/', // Base URL for GitHub Pages repository 'site'
+    base: '/openneural/', // Base URL for GitHub Pages repository 'openneural'
 })
